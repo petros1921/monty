548ad8a3902b438f579b8898603dd9c53a4026fe
petros1921 <petrosworku19@gmail.com>
2023-12-14 04:53:27 -0800

    fix swap

--- st_swap.c
+++ st_swap.c
@@ -10,24 +10,24 @@
 void st_swap(stack_t **entry, unsigned int shelf)
 {
 	stack_t *top_node;
-	int len = 0, aux;
+	int count = 0, swap;
 
 	top_node = *entry;
 	while (top_node)
 	{
 		top_node = top_node->next;
-		len++;
+		count++;
 	}
-	if (len < 2)
+	if (count < 2)
 	{
 		fprintf(stderr, "L%d: cannot swap, stack is to short\n", shelf);
-		fclose(bus.file);
-		free(bus.content);
+		fclose(monty_con.file);
+		free(monty_con.file_content);
 		exit(EXIT_FAILURE);
 	}
 
 	top_node = *entry;
-	aux = top_node->n;
+	swap = top_node->n;
 	top_node->n = top_node->next->n;
-	top_node->next->n = aux;
+	top_node->next->n = swap;
 }
