06031b46ecbb9ab1f8e4f3596b2599dccf487c3d
Kidus <kingkidy50@gmail.com>
2023-12-14 09:17:58 -0800

    addition

--- execute_by.c
+++ execute_by.c
@@ -4,38 +4,58 @@
  * execute_bytecode - Execute bytecode instructions.
  * @entry: Pointer to the entry point of the stack.
  * By Kidus Yohannes and Petros Worku.
+ *
  */
-void execute_bytecode(stack_t **entry)
+
+void execute_bytecode(char *content, stack_t **entry, unsigned int line_number, FILE *file)
 {
-	char *token;
-	const char delimiters[] = " \t\n";
-	const char *opcode;
-	int value;
+	instruction_t instructions[] = {
+		{"push", st_push},
+		{"pall", st_pall},
+		{"pint", st_pint},
+		{"pop", st_pop},
+		{"swap", st_swap},
+		{"add", st_add},
+		{"nop", st_nop},
+		{"sub", st_sub},
+		{"div", st_div},
+		{"mul", st_mul},
+		{"mod", st_mod},
+		{"pchar", st_pchar},
+		{"pstr", st_pstr},
+		{"rotl", st_rotl},
+		{"rotr", st_rotr},
+		{"queue", st_queue},
+		{"stack", st_stack},
+		{NULL, NULL}
+	};
 
-	token = strtok(monty_con.file_content, delimiters);
-	while (token != NULL)
+	unsigned int index = 0;
+	char *opcode;
+
+	opcode = strtok(content, " \n\t");
+	if (opcode && opcode[0] == '#')
+		return (0);
+
+	monty_con.argument = strtok(NULL, " \n\t");
+
+	while (instructions[index].opcode && opcode)
 	{
-		opcode = token;
-
-		token = strtok(NULL, delimiters);
-		if (token != NULL)
+		if (strcmp(opcode, instructions[index].opcode) == 0)
 		{
-			value = atoi(token);
+			instructions[index].f(entry, line_number);
+			return (0);
 		}
-		else
-		{
-			value = 0;
-		}
-
-		if (strcmp(opcode, "push") == 0)
-		{
-			st_push(entry, value);
-		}
-		else if (strcmp(opcode, "pall") == 0)
-		{
-			st_pall(entry, 0);
-		}
-
-		token = strtok(NULL, delimiters);
+		index++;
 	}
+
+	if (opcode && instructions[index].opcode == NULL)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+		fclose(file);
+		free(content);
+		stack_remover(*entry);
+		exit(EXIT_FAILURE);
+	}
+	return (1);
 }
